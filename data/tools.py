e0aa9704590be2a995a5b102fe02b67a0424db23
Robson Marinho <robson_luz15@hotmail.com>
2018-10-29 18:38:18 -0300

    Criando a classe de inicialização

--- data/tools.py
+++ data/tools.py
@@ -16,3 +16,13 @@ keybiding = {
 
 }
 
+#CLASSE PRINCIPAL QUE CONTROLA O JOGO, CONTÉM O LOOP DO JOGO E OS EVENTOS
+
+def __init__(self, caption):
+    self.screen = pg.display.get_surface()  #Verifica a posição da tela
+    self.done = False   #Verifica se está jogando ou não
+    self.Clock = pg.time.Clock()    #verifica o tempo do jogo
+    self.caption = caption
+    self.fdp = 60   #Controla a velocidade do jogo
+    self.show_fps = False
+    self.current_time = 0.0 #Tempo zerado ao (re)iniciar
