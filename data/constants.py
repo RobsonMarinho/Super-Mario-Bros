d078a239c53fe82ad54e85d5b358855c46e47068
Robson Marinho <robson_luz15@hotmail.com>
2018-10-29 18:48:35 -0300

    Criando as constantes

--- data/constants.py
+++ data/constants.py
@@ -1,1 +1,17 @@
 __author__ = 'Robson_Marinho'
+
+
+### CRIA TELA / CORES / TAMANHOA / POSIÇÃO / VELOCIDADE ###
+
+SREEN_HEIGHT = 600  #Altura da tela
+SREEN_WIDTH = 800   #largura da tela
+
+SREEN_SIZE = (SREEN_WIDTH,SREEN_HEIGHT) #Variável de tamanho e largura
+
+TITLE = "Super Mário Bros"  #Títtulo da página
+
+#### CORES RGB ###
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GRAY = (100, 100, 100)
