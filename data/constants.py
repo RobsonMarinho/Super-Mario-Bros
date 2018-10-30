b32ac9507a9d9e31f461ccbdaccc494819673fca
Robson Marinho <robson_luz15@hotmail.com>
2018-10-29 23:09:08 -0300

    Configurando a tela inicial

--- data/constants.py
+++ data/constants.py
@@ -10,8 +10,21 @@ SREEN_SIZE = (SREEN_WIDTH,SREEN_HEIGHT) #Variável de tamanho e largura
 
 TITLE = "Super Mário Bros"  #Títtulo da página
 
-#### CORES RGB ###
+    #### CORES RGB ###
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GRAY = (100, 100, 100)
+NAVYBLUE = (60, 60, 100)
+GREEN = ( 0, 255, 0)
+FOREST_GREEN = (31, 162, 35)
+BLUE = (0, 0, 255)
+SKY_BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 255, 0)
+PURPLE = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+NEAR__BLACK = (19, 15, 48)
+COMBLUE = (233, 232, 255)
+GOLD = (255, 215, 0)
--- data/setup.py
+++ data/setup.py
@@ -1,9 +1,23 @@
 __author__ = 'Robson_Marinho'
 
-import pygame  as pg
-import config
+import os   #centraliza tela com essa classe que é usada em IOS/Android
+import pygame  as pg            #importa pygame
+from . import tools             #importa calsse tools
+from . import constants as c    #importa a constants
 
-#CRIA A TELA E DIRETÓRIOS DE RECURSOS
+    ### CRIANDO A TELA E DIRETÓRIOS DE RECURSOS ###
+
+TITLE = c.TITLE #get Título do jogo
+
+os.environ['SDL VIDEO CENTERED']    #centraliza a tela
+pg.ini()    #inicializa
+
+#Eventos de abaixar, parar, e sair
+pg.event.set_allowed([pg.KEYDOWN, pg.KEYUP, pg.QUIT])
+
+pg.display.set_caption(c.TITLE)
+SCREEN = pg.display.set_mode(c.SCREEN_SIZE)
+SCREEN_RECT = SCREEN.get_rect() #Recebe área retangular
 
 FINTS = ''  #Variável constante
 MUSIC = ''  #Variável de musicas
