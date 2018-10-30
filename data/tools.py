7d0e524f095f640217b69e58a9c7720b30e74900
Robson Marinho <robson_luz15@hotmail.com>
2018-10-30 02:24:49 -0300

    classe principal do jogo

--- data/tools.py
+++ data/tools.py
@@ -43,7 +43,7 @@ def update(self):
     if self.state.quit: #se fecha a tela do jogo, se torna True
         sel.done = True
     elif self.state.done:
-        self.flip_state()   
+        self.flip_state()
     self.state.update(self.screen, self.keys, self.current_time)
 
     def setup_states(self, state_dict, start_state):
@@ -57,8 +57,42 @@ def update(self):
         if self.state.quit:
             self.done = True
         elif self.state.done:
-            self.flip_state()
+            self.flip_state()   #Verificando o estado do menu (a troca de player 1 para player 2)
         self.state.update(self.screen, self.keys, self.current_time)
 
+    ''' TROCA DE ESTADOS DO MENU '''
     def flip_state(self):
         previous, self.state_name = self.state_name, self.state.next
+        persist = self.state.cleanup()
+        self.state = self.state_dict[self.state_name]
+        self.state.startup(self.current_time, persist)
+        self.state.previous = previous
+
+    #Função onde se apertar o 'x' para sair, o done fica True e executa a função event_loop
+    def event_loop(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.done = True
+            elif event.type == pg.KEYDOWN:  #Verifica se alguma telca foi pressionada
+                self.keys = pg.key.get_pressed()
+                self.toggle_show_fps(event.key)
+            elif event.type == pg.KEYUP:
+                self.keys = pg.key.get_pressed()
+                self.state.get_event(event)
+
+    def toggle_show_fps(self, key):
+        if key == pg.K_F5:
+            self.show_fps = not self.show_fps   #Não exibe o título
+            if not self.show_fps:
+                pg.display.set_caption(self.caption)
+
+    def main(self):
+        """Main lloop for entire program"""
+        while not self.done:
+            self.event_loop()
+            self.update()
+            pg.display.update()
+            if self.show_fps:   #Se for True executa o clock e exibe os frames por segundo
+                fps = self.clock.get_fps()
+                with_fps = "{} - {:.2f} FPS".format(self.caption, fps)
+                pg.display.set_caption(with_fps)
