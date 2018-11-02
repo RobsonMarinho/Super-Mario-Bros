0725eb56458fc0680b6f0edf6b784023c3587a20
Robson Marinho <robson_luz15@hotmail.com>
2018-11-02 04:00:34 -0300

    Criando o arquivo main principal

--- data/main.py
+++ data/main.py
@@ -2,3 +2,8 @@ __author__ = 'Robson_Marinho && Isabela Oliveira'
 
 import tools, setup
 import constants as c
+
+def main():
+    ## Adicionar os estados e a classe de controle ##
+    run_it = tools.Control(setup.TITLE) #Chama o título
+    run_it.main()
--- mario_level01.py
+++ mario_level01.py
@@ -1,2 +1,11 @@
-__author__ = 'Robson_Marinho'
+__author__ = 'Robson_Marinho && Isabela Oliveira'
 
+import sys
+import pygame as pg
+from data.main import main
+import cProfile
+
+if __name__ =='__main__':
+    main()
+    pg.quit()
+    sys.exit()
