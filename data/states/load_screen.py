59305131d15fe207ef8cb4a7f886704da34483e7
Robson Marinho <robson_luz15@hotmail.com>
2018-11-03 02:12:20 -0300

    Criando arquivo info e classes no load_screen.py

--- data/states/load_screen.py
+++ data/states/load_screen.py
@@ -3,9 +3,10 @@ __author__ = 'Robson_Marinho && Isabela Oliveira'
 from .. import setup, tools
 from .. import constants as c
 from .. import game_sound
-from .. components import info
+from ..components import info
 
-    ### Recebe o estado de carregamento ###
+
+### Recebe o estado de carregamento ###
 class LoadScreen(tools._State):
     def __init__(self):
         tools._State.__init__(self)
@@ -30,11 +31,79 @@ class LoadScreen(tools._State):
         return c.LOAD_SCREEN
 
     def update(self, surface, keys, current_time):
-        """Updates the loading screen"""
-        if (current)time - self.start_time) < 2400:
+        """Atualizando a tela"""
+        if (current_time - self.start_time) < 2400:
             surface.fill(c.BLACK)
-            self.overhead_into.update(self.game_info)
+            self.overhead_info.update(self.game_info)  # Pede para atualizar
             self.overhead_info.draw(surface)
 
         elif (current_time - self.start_time) < 2600:
             surface.fill(c.BLACK)
+
+        elif (current_time - self.start_time) < 2635:
+            surface.fill((106, 150, 252))
+
+        else:
+            self.done = True
+
+    ### CLASSE GAME OVER ###
+
+
+class GameOver(LoadScreen):
+    """Carregamento da tela de game over"""
+
+    def __init__(self):
+        super(GameOver, self).__init__()
+
+    def set_next_state(self):
+        """Configurando proximo estado"""
+        return c.MAIN_MENU
+
+    def set_overhead_info_state(self):
+        """Atualizando para fim do jogo"""
+        return c.GAME_OVER
+
+    def update(self, surface, keys, current_time):
+        self.current_time = current_time
+        self.sound_manager.update(self.persist, None)
+
+        if (self.current_time - self.start_time) < 7000:
+            surface.fill(c.BLACK)
+            self.overhead_info.update(self.game_info)
+            self.overhead_info.draw(surface)
+        elif (self.current_time - self.start_time) < 7200:
+            surface.fill(c.BLACK)
+        elif (self.current_time - self.start_time) < 7235:
+            surface.fill((106, 150, 252))
+        else:
+            self.done = True
+
+    ### Classe Time out ###
+
+
+class TimeOut(LoadScreen):
+    """Carregando tela com fim do tempo"""
+
+    def __init__(self):
+        super(TimeOut, self).__init__()
+
+    def set_next_state(self):
+        """Passando para o proximo estagio"""
+        if self.persist[c.LIVES] == 0:
+            return c.GAME_OVER
+        else:
+            return c.LOAD_SCREEN
+
+    def set_overhead_info_state(self):
+        """Definindo o estágio com fim de tempo"""
+        return c.TIME_OUT
+
+    def update(self, surface, keys, current_time):
+        self.current_time = current_time
+
+        if (self.current_time - self.start_time) < 2400:
+            surface.fill(c.BLACK)
+            self.overhead_info.update(self.game_info)
+            self.overhead_info.draw(surface)
+        else:
+            self.done = True
